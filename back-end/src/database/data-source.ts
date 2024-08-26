ad7226134aa58c7a764865bb118cda5acd021c9a
luiz2k <luizteles934@gmail.com>
2024-08-26 20:02:37 -0300

    Importar as entidades do banco de dados para o data-source

--- back-end/src/database/data-source.ts
+++ back-end/src/database/data-source.ts
@@ -1,12 +1,15 @@
 import "dotenv/config";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { Session } from "./entities/sessionEntity";
+import { Task } from "./entities/taskEntity";
+import { User } from "./entities/userEntity";
 
 export const AppDataSource = new DataSource({
 	type: "sqlite",
 	database: "src/database/sqlite.db",
 	synchronize: false,
 	logging: false,
-	entities: [],
+	entities: [User, Task, Session],
 	migrations: [],
 });
