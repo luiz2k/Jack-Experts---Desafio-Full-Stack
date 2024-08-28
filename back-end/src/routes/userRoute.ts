52ca272a24803c3bb12139809332e12a573fb0ca
luiz2k <luizteles934@gmail.com>
2024-08-28 16:36:38 -0300

    Implementar rotas responsável pelo usuário

---
+++ back-end/src/routes/userRoute.ts
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import { UserController } from "../controllers/userController";
+import { UserRepository } from "../repositories/userRepository";
+import { UserService } from "../services/userService";
+
+// Rotas relacionadas aos usuários
+class UserRoutes {
+	private router: Router;
+	private userController: UserController;
+
+	constructor() {
+		const userRepository = new UserRepository();
+		const userService = new UserService(userRepository);
+
+		this.userController = new UserController(userService);
+
+		this.router = Router();
+
+		this.routes();
+	}
+
+	// Configura as rotas
+	private routes() {
+		this.router.post("/", this.userController.create.bind(this.userController));
+	}
+
+	// Retorna as rotas configuradas
+	public getRoutes() {
+		return this.router;
+	}
+}
+
+export const userRoutes = new UserRoutes();
