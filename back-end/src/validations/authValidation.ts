4e483eafd6eb93ee49fbcf83bf908622d5f98dc6
luiz2k <luizteles934@gmail.com>
2024-08-28 19:41:57 -0300

    Criar esquema para validação de autenticação

---
+++ back-end/src/validations/authValidation.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+	email: z
+		.string()
+		.email("Informe um e-mail válido.")
+		.transform((email) => email.toLowerCase()),
+	password: z.string().min(6, "A senha deve ter pelo menos 6 digitos."),
+});
+
+export const logoutSchema = z.string();
