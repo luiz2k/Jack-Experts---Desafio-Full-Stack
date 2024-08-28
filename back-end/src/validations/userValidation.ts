e7c4a13d2750469cc6aa8469fcd3c5bd94d68564
luiz2k <luizteles934@gmail.com>
2024-08-28 16:12:19 -0300

    Criar esquema de válidação para a criação de um novo usuário

---
+++ back-end/src/validations/userValidation.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export const createUserSchema = z
+	.object({
+		email: z
+			.string()
+			.email("Informe um e-mail válido.")
+			.transform((email) => email.toLowerCase()),
+		password: z.string().min(6, "A senha deve ter pelo menos 6 digitos."),
+		confirmPassword: z.string(),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "As senhas não coincidem.",
+		path: ["confirmPassword"],
+	});
