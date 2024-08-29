a9322b37622a57d82f7dd8fb15413c4c942617c7
luiz2k <luizteles934@gmail.com>
2024-08-29 11:08:33 -0300

    Criar enum responsável por representar statusCode da aplicação

---
+++ back-end/src/helpers/httpStatusCode.ts
@@ -0,0 +1,12 @@
+// Enum para representar o status de uma requisição HTTP
+export enum HttpStatusCode {
+	OK = 200,
+	CREATED = 201,
+	NO_CONTENT = 204,
+	BAD_REQUEST = 400,
+	UNAUTHORIZED = 401,
+	FORBIDDEN = 403,
+	NOT_FOUND = 404,
+	CONFLICT = 409,
+	INTERNAL_SERVER_ERROR = 500,
+}
