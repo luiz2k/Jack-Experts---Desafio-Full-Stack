ccfb2c60ba4150237d11c6c856c12a5f44894656
luiz2k <luizteles934@gmail.com>
2024-08-29 12:31:25 -0300

    Gerar migrations

--- back-end/src/database/data-source.ts
+++ back-end/src/database/data-source.ts
@@ -4,12 +4,13 @@ import { DataSource } from "typeorm";
 import { Session } from "./entities/sessionEntity";
 import { Task } from "./entities/taskEntity";
 import { User } from "./entities/userEntity";
+import { Init1724941889387 } from "./migrations/1724941889387-init";
 
 export const AppDataSource = new DataSource({
 	type: "sqlite",
-	database: "src/database/sqlite.db",
+	database: "db/sqlite.db",
 	synchronize: false,
 	logging: false,
 	entities: [User, Task, Session],
-	migrations: [],
+	migrations: [Init1724941889387],
 });
