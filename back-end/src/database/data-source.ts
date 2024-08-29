1e332c609d52e5f82036775c5fc3c91f3dcc34f5
luiz2k <luizteles934@gmail.com>
2024-08-29 12:43:30 -0300

    Configurar o diretório do database, entities e migrations

--- back-end/src/database/data-source.ts
+++ back-end/src/database/data-source.ts
@@ -1,16 +1,12 @@
 import "dotenv/config";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { Session } from "./entities/sessionEntity";
-import { Task } from "./entities/taskEntity";
-import { User } from "./entities/userEntity";
-import { Init1724941889387 } from "./migrations/1724941889387-init";
 
 export const AppDataSource = new DataSource({
 	type: "sqlite",
-	database: "db/sqlite.db",
+	database: ".db/sqlite.db",
 	synchronize: false,
 	logging: false,
-	entities: [User, Task, Session],
-	migrations: [Init1724941889387],
+	entities: [`${__dirname}/entities/*{.ts,.js}`],
+	migrations: [`${__dirname}/migrations/*{.ts,.js}`],
 });
