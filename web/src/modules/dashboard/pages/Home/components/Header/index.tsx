1baf9dda8629d466dd7e3c9ce184fcbecd422d5f
luiz2k <luizteles934@gmail.com>
2024-09-01 11:51:49 -0300

    Criar header da aplicação

---
+++ web/src/modules/dashboard/pages/Home/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { BookOpenCheck } from "lucide-react";
+import { Link } from "react-router-dom";
+import SearchInput from "./components/SearchInput";
+import { SignOutButton } from "./components/SignOutButton";
+
+export function Header() {
+	return (
+		<header className="p-5 gap-2.5 border-b bg-primary flex items-center justify-between">
+			<Link to="/" className="uppercase text-secondary">
+				<BookOpenCheck size={35} />
+			</Link>
+
+			<div className="flex gap-2.5 items-center">
+				<SearchInput />
+
+				<SignOutButton />
+			</div>
+		</header>
+	);
+}
