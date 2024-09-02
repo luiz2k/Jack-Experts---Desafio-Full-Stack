2d8362ff2c0085e561cca697de23e5e69f746624
luiz2k <luizteles934@gmail.com>
2024-09-02 15:08:26 -0300

    Criar um serviço para buscar um usuário pelo e-mail

--- back-end/src/services/interfaces/IUserService.ts
+++ back-end/src/services/interfaces/IUserService.ts
@@ -3,11 +3,19 @@ export type UserInput = {
 	password: string;
 };
 
-export type UserOutput = {
+export type CreateOutput = {
 	id: string;
 	email: string;
 };
 
+export type FindByEmailOutput = {
+	id: string;
+	email: string;
+	password: string;
+};
+
 export interface IUserService {
-	create(data: UserInput): Promise<UserOutput>;
+	create(data: UserInput): Promise<CreateOutput>;
+
+	findByEmail(email: string): Promise<FindByEmailOutput | null>;
 }
--- back-end/src/services/userService.ts
+++ back-end/src/services/userService.ts
@@ -3,19 +3,20 @@ import { ConflictError } from "../helpers/errorHandler";
 
 import type { IUserRepository } from "../repositories/interfaces/IUserRepository";
 import type {
+	CreateOutput,
+	FindByEmailOutput,
 	IUserService,
 	UserInput,
-	UserOutput,
 } from "./interfaces/IUserService";
 
 // Serviço responsável por lidar com a regra de negócio dos usuários
 export class UserService implements IUserService {
-	constructor(private readonly UserRepository: IUserRepository) {}
+	constructor(private readonly userRepository: IUserRepository) {}
 
 	// Verifica se o usuário existe no sistema
 	// Se não existir, faz a criptografia da senha e salva o usuário no sistema
-	async create(data: UserInput): Promise<UserOutput> {
-		const userExists = await this.UserRepository.findByEmail(data.email);
+	async create(data: UserInput): Promise<CreateOutput> {
+		const userExists = await this.userRepository.findByEmail(data.email);
 
 		if (userExists) {
 			throw new ConflictError("E-mail já cadastrado.");
@@ -25,7 +26,7 @@ export class UserService implements IUserService {
 
 		const hashPassword = bcryptjs.hashSync(data.password, SALT);
 
-		const user = await this.UserRepository.create({
+		const user = await this.userRepository.create({
 			...data,
 			password: hashPassword,
 		});
@@ -34,5 +35,12 @@ export class UserService implements IUserService {
 			id: user.id,
 			email: user.email,
 		};
+	}
+
+	// Busca um usuário pelo seu e-mail
+	async findByEmail(email: string): Promise<FindByEmailOutput | null> {
+		const user = await this.userRepository.findByEmail(email);
+
+		return user;
 	}
 }
