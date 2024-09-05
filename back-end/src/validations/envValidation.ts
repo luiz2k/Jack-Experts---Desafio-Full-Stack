cca0de550db6e0ec13fe01c7ef4a218e8f1d5f25
luiz2k <luizteles934@gmail.com>
2024-09-05 10:31:14 -0300

    Melhorar as mensagens da validação das variáveis de ambiente

--- back-end/src/validations/envValidation.ts
+++ back-end/src/validations/envValidation.ts
@@ -1,14 +1,30 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-	PORT: z.string().transform((port) => Number(port)),
-	CORS: z.string().url(),
+	PORT: z
+		.string({
+			message: "A porta deve ser informada.",
+		})
+		.transform((port) => Number(port)),
+	CORS: z
+		.string({
+			message: "A URL de origem deve ser informada.",
+		})
+		.url({
+			message: "A URL de origem deve ser uma URL válida.",
+		}),
 
-	TOKEN_SECRET: z.string(),
-
-	SWAGGER_ROUTE: z.string().regex(/^\/[a-zA-Z]+$/, {
-		message: "O caminho deve começar com '/' seguido de letras apenas.",
+	TOKEN_SECRET: z.string({
+		message: "Uma segredo para o JWT deve ser informado.",
 	}),
+
+	SWAGGER_ROUTE: z
+		.string({
+			message: "O caminho para a rota do Swagger deve ser informado.",
+		})
+		.regex(/^\/[a-zA-Z]+$/, {
+			message: "O caminho deve começar com '/' seguido de letras. Ex: '/docs'.",
+		}),
 });
 
 export const env = envSchema.parse(process.env);
